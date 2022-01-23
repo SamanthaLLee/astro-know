69d7a315c7f2ab07b684230e25b0c0d34c1ae6b2
SamanthaLLee <samantha.lin.lee@gmail.com>
2022-01-22 23:20:35 -0500

    lots of attempts to extract wiki

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-// astronaut details
+
+const start = async function (a, b) {
+    const url = "https://en.wikipedia.org/w/api.php?" +
+        new URLSearchParams({
+            origin: "*",
+            action: "parse",
+            page: "List of Hispanic astronauts",
+            format: "json",
+        });
+
+    try {
+        const req = await fetch(url);
+        const json = await req.json();
+        console.log(json.parse.text["*"]);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+start();
